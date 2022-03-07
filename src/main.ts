7cd712c5255317e41376d190e51145d5cf701107
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-07 10:44:46 -0300

    Add class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,28 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto implements User {
+  @IsEmail({ message: 'Must be an e-mail' })
+  email: string;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must have length of at least 8' })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[a-z]).*$/, {
+    message: 'Password must contain at least 1 number and 1 letter',
+  })
+  password: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  address?: string;
+}
