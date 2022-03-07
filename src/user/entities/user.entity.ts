59cad81b995be6c4eae887f2cd8dbdd8524b3c24
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-07 10:44:25 -0300

    Init file

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { Prisma } from '@prisma/client';
+
+export class User implements Prisma.UserUncheckedCreateInput {
+  id?: string;
+  email: string;
+  password: string;
+  name?: string;
+  address?: string;
+  createdAt?: string | Date;
+  updatedAt?: string | Date;
+}
