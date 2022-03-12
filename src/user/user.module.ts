0d6235cee76f31b920e03c95558f1d05abf8dd6e
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-12 13:08:29 -0300

    Import prisma service
    Needed for the tests to find the prisma module, if prisma was global the tests could not find it

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
+  imports: [PrismaModule],
   exports: [UserService],
 })
 export class UserModule {}
