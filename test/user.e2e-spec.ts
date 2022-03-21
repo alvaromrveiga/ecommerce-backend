d2e173f3fe4bb9ca5c5f62316f4e8967ddb3c13d
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-21 12:49:15 -0300

    Should throw email already in use error

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,6 +1,8 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthModule } from 'src/auth/auth.module';
+import { EmailInUseError } from 'src/errors/email-in-use.error';
+import { PrismaInterceptor } from 'src/interceptors/prisma.interceptor';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserModule } from 'src/user/user.module';
 import * as request from 'supertest';
@@ -24,18 +26,17 @@ describe('UserController (e2e)', () => {
       }),
     );
 
+    app.useGlobalInterceptors(new PrismaInterceptor());
+
     await app.init();
 
     const prisma = app.get<PrismaService>(PrismaService);
     await prisma.user.deleteMany();
 
-    await request(app.getHttpServer())
-      .post('/user')
-      .send({
-        email: 'tester0@example.com',
-        password: 'abc123456',
-      })
-      .expect(201);
+    await request(app.getHttpServer()).post('/user').send({
+      email: 'tester0@example.com',
+      password: 'abc123456',
+    });
 
     const response = await request(app.getHttpServer())
       .post('/login')
@@ -56,13 +57,17 @@ describe('UserController (e2e)', () => {
     });
 
     it('should not create user if email is already in use', () => {
-      return request(app.getHttpServer())
-        .post('/user')
-        .send({
-          email: 'tester0@example.com',
-          password: 'abc123456',
-        })
-        .expect(500);
+      return expect(
+        request(app.getHttpServer())
+          .post('/user')
+          .send({
+            email: 'tester0@example.com',
+            password: 'abc123456',
+          })
+          .expect(400),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(new EmailInUseError().getResponse()),
+      });
     });
 
     it('should not create user if email is invalid', () => {
