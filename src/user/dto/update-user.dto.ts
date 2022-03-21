381cfe9b1f6db156bac3fdad36e1273f03e8f006
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-21 19:42:30 -0300

    Ask for current password to update it

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  currentPassword?: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { hash } from 'bcrypt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { hash, compare } from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -31,6 +31,8 @@ export class UserService {
   }
 
   async update(id: string, updateUserDto: UpdateUserDto): Promise<User> {
+    await this.hashIfUpdatingPassword(id, updateUserDto);
+
     const user = await this.prisma.user.update({
       where: { id },
       data: { ...updateUserDto },
@@ -41,5 +43,29 @@ export class UserService {
 
   async remove(id: string): Promise<void> {
     await this.prisma.user.delete({ where: { id } });
+  }
+
+  private async hashIfUpdatingPassword(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<void> {
+    if (updateUserDto.password && updateUserDto.currentPassword) {
+      const user = await this.prisma.user.findUnique({ where: { id } });
+
+      const isCorrectPassword = await compare(
+        updateUserDto.currentPassword,
+        user.password,
+      );
+
+      if (!isCorrectPassword) {
+        throw new UnauthorizedException('Invalid current password');
+      }
+
+      const hashedPassword = await hash(updateUserDto.password, 10);
+
+      updateUserDto.password = hashedPassword;
+    }
+
+    delete updateUserDto.currentPassword;
   }
 }
