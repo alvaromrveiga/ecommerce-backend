cf617efc68e470e7260aecb5b912ce2c86ab2caf
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-22 10:53:52 -0300

    Make currentPassword optional

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsString()
+  @IsOptional()
   currentPassword?: string;
 }
