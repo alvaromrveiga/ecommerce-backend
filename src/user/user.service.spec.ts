96aae45cb80800d177ad2c9ab12fabd0fa9b20a1
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-22 10:52:28 -0300

    Add update tests and reset beforeEach

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,17 +1,17 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
-const userArray: User[] = [];
+let userArray: User[] = [];
 
 describe('UserService', () => {
   let userService: UserService;
   let prismaService: PrismaService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
@@ -28,13 +28,13 @@ describe('UserService', () => {
                 });
               }),
               update: jest.fn().mockImplementation(({ where, data }) => {
-                let user = userArray.find((user) => {
+                const userIndex = userArray.findIndex((user) => {
                   return user.email === where.id || user.email === where.email;
                 });
 
-                user = { ...user, ...data };
+                userArray[userIndex] = { ...userArray[userIndex], ...data };
 
-                return user;
+                return userArray[userIndex];
               }),
               delete: jest.fn().mockImplementation(({ where }) => {
                 const userIndex = userArray.findIndex((user) => {
@@ -53,6 +53,25 @@ describe('UserService', () => {
     prismaService = module.get<PrismaService>(PrismaService);
   });
 
+  beforeEach(async () => {
+    userArray = [];
+
+    await userService.create({
+      email: 'tester@example.com',
+      password: 'abc123456',
+    });
+
+    await userService.create({
+      email: 'tester2@example.com',
+      password: 'abc123456',
+    });
+
+    await userService.create({
+      email: 'tester3@example.com',
+      password: 'abc123456',
+    });
+  });
+
   it('should be defined', () => {
     expect(userService).toBeDefined();
     expect(prismaService).toBeDefined();
@@ -60,32 +79,16 @@ describe('UserService', () => {
 
   describe('createUser', () => {
     it('should createUser', async () => {
-      const data: CreateUserDto = {
-        email: 'tester@example.com',
-        password: 'abc123456',
-      };
-
-      await userService.create(data);
-
-      const data2: CreateUserDto = {
-        email: 'tester2@example.com',
-        password: 'abc123456',
-      };
-
-      await userService.create(data2);
-
-      const data3: CreateUserDto = {
-        email: 'tester3@example.com',
-        password: 'abc123456',
-      };
-
-      await userService.create(data3);
+      await userService.create({
+        email: 'tester4@example.com',
+        password: 'abcd1234567',
+      });
 
       expect(prismaService.user.create).toHaveBeenCalled();
 
-      expect(userArray.length).toEqual(3);
-      expect(userArray[0].email).toEqual('tester@example.com');
-      expect(userArray[0].password).not.toEqual('abc123456');
+      expect(userArray.length).toEqual(4);
+      expect(userArray[3].email).toEqual('tester4@example.com');
+      expect(userArray[3].password).not.toEqual('abcd1234567');
     });
   });
 
@@ -113,20 +116,77 @@ describe('UserService', () => {
 
   describe('update', () => {
     it('should update user', async () => {
-      const data: UpdateUserDto = { name: 'Tester2' };
+      const data: UpdateUserDto = {
+        name: 'Tester2',
+        address: 'World Street 0 House 0',
+        email: 'tester2_new@example.com',
+        currentPassword: 'abc123456',
+        password: 'abc1234567',
+      };
 
       const user = await userService.update('tester2@example.com', data);
 
       expect(prismaService.user.update).toHaveBeenCalled();
 
-      expect(user.email).toEqual('tester2@example.com');
-      expect(user.password).not.toEqual('abc123456');
+      expect(user.email).toEqual('tester2_new@example.com');
       expect(user.name).toEqual('Tester2');
+      expect(user.address).toEqual('World Street 0 House 0');
+
+      expect(user.password).toBeUndefined();
+      expect(user).not.toHaveProperty('currentPassword');
+
+      const userWithPassword = await userService.findByEmail(
+        'tester2_new@example.com',
+      );
+
+      expect(userWithPassword.password).not.toEqual('abc123456');
+      expect(userWithPassword.password).not.toEqual('abc1234567');
+    });
+
+    it('should not update if current password is invalid', async () => {
+      const data: UpdateUserDto = {
+        currentPassword: 'wrongPassword123',
+        password: 'abc12345678',
+      };
+
+      await expect(
+        userService.update('tester2@example.com', data),
+      ).rejects.toThrow(new BadRequestException('Invalid current password'));
+    });
+
+    it('should not update if current password is empty', async () => {
+      const data: UpdateUserDto = {
+        password: 'abc12345678',
+      };
+
+      await expect(
+        userService.update('tester2@example.com', data),
+      ).rejects.toThrow(
+        new BadRequestException(
+          'Please enter both new password and current password',
+        ),
+      );
+    });
+
+    it('should not update if password is empty', async () => {
+      const data: UpdateUserDto = {
+        currentPassword: 'abc1234567',
+      };
+
+      await expect(
+        userService.update('tester2@example.com', data),
+      ).rejects.toThrow(
+        new BadRequestException(
+          'Please enter both new password and current password',
+        ),
+      );
     });
   });
 
   describe('delete', () => {
     it('should remove user', async () => {
+      console.log(userArray);
+
       await userService.remove('tester2@example.com');
 
       expect(prismaService.user.delete).toHaveBeenCalled();
@@ -138,6 +198,7 @@ describe('UserService', () => {
       });
 
       expect(user).toEqual(undefined);
+      console.log(userArray);
     });
   });
 });
