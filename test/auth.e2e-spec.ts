6594ee178df4fc16dd2809655e6a266337b26789
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-25 15:34:22 -0300

    Add auth e2e tests

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,100 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { JwtService } from '@nestjs/jwt';
+import { isJWT } from 'class-validator';
+import { AppModule } from 'src/app.module';
+import { PrismaInterceptor } from 'src/interceptors/prisma.interceptor';
+import { PrismaService } from 'src/prisma/prisma.service';
+import request from 'supertest';
+import ms from 'ms';
+import { jwtConfig } from 'src/config/jwt.config';
+
+describe('AuthController (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwtService: JwtService;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
+
+    app.useGlobalInterceptors(new PrismaInterceptor());
+
+    await app.init();
+
+    prisma = app.get<PrismaService>(PrismaService);
+    jwtService = app.get<JwtService>(JwtService);
+  });
+
+  beforeEach(async () => {
+    await prisma.user.deleteMany();
+
+    await request(app.getHttpServer()).post('/user').send({
+      email: 'tester0@example.com',
+      password: 'abc123456',
+    });
+
+    await request(app.getHttpServer()).post('/user').send({
+      email: 'tester1@example.com',
+      password: 'abc123456',
+    });
+  });
+
+  describe('Post /login', () => {
+    it('should login', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: 'tester0@example.com',
+          password: 'abc123456',
+        })
+        .expect(200);
+
+      expect(response.body).toHaveProperty('accessToken');
+      expect(isJWT(response.body.accessToken)).toBeTruthy();
+
+      const user = await prisma.user.findUnique({
+        where: { email: 'tester0@example.com' },
+      });
+
+      const { sub, iat, exp } = jwtService.verify(response.body.accessToken);
+
+      expect(sub).toEqual(user.id);
+
+      const expiresInSeconds = ms(jwtConfig.signOptions.expiresIn) / 1000;
+
+      expect(exp).toEqual(iat + expiresInSeconds);
+    });
+
+    it('should not login user if password is wrong', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: 'tester0@example.com',
+          password: 'wrongPassword',
+        })
+        .expect(401);
+    });
+
+    it('should not login user if email does not exist', () => {
+      return request(app.getHttpServer())
+        .post('/login')
+        .send({
+          email: 'unexistentTester@example.com',
+          password: 'abc123456',
+        })
+        .expect(401);
+    });
+  });
+});
