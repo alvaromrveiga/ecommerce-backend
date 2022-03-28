6205c2505b4cee899fdbd78ae94eb3f95223e1c6
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-03-28 11:20:11 -0300

    Add class-validator validations

--- src/auth/dto/login-credentials.dto.ts
+++ src/auth/dto/login-credentials.dto.ts
@@ -1,13 +1,19 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class LoginCredentialsDto {
   /**
    * User email
    * @example "user@example.com"
    */
+  @IsNotEmpty()
+  @IsString()
   email: string;
 
   /**
    * User password
    * @example "abc123456"
    */
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
