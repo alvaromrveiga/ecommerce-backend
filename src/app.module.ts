de589d1e6ae55aafc065ae8fcdfc856837e625f7
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-03 11:40:32 -0300

    Generate product CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { UserModule } from './models/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ProductModule } from './models/product/product.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule],
+  imports: [UserModule, AuthModule, PrismaModule, ProductModule],
   providers: [
     {
       provide: APP_GUARD,
---
+++ src/models/product/product.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductController } from './product.controller';
+
+@Module({
+  controllers: [ProductController],
+  providers: [ProductService]
+})
+export class ProductModule {}
