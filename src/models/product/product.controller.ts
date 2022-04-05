c8afbd8c42e366ebd1b04c8d6c2fae6a57468498
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-05 11:08:50 -0300

    Add swagger docs product tag

--- src/models/product/product.controller.ts
+++ src/models/product/product.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('product')
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
