e371d2c4642f29bc07d6b7296bb29068732871ff
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-04-05 11:09:01 -0300

    Import PrismaModule

--- src/models/product/product.module.ts
+++ src/models/product/product.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService]
+  imports: [PrismaModule],
+  providers: [ProductService],
 })
 export class ProductModule {}
