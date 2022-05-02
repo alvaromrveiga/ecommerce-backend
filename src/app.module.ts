be7611b1d52850aa0d47bd31f068f80d8736899a
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-02 15:07:17 -0300

    Generate nest category CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,16 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { UserModule } from './models/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './models/product/product.module';
+import { CategoryModule } from './models/category/category.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PrismaModule, ProductModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    ProductModule,
+    CategoryModule,
+  ],
   providers: [
     {
       provide: APP_GUARD,
