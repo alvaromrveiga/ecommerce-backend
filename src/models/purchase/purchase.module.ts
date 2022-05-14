82473c87f04dccb40213e521bd75d8159186a3dd
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-14 13:18:40 -0300

    Import prisma module

--- src/models/purchase/purchase.module.ts
+++ src/models/purchase/purchase.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PurchaseService } from './purchase.service';
 import { PurchaseController } from './purchase.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [PurchaseController],
-  providers: [PurchaseService]
+  imports: [PrismaModule],
+  providers: [PurchaseService],
 })
 export class PurchaseModule {}
