1a95c8ff2cfd8bc1dde0343cd99592160c1dcbfa
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-17 09:50:58 -0300

    Validate produtId as uuid v4

--- src/models/purchase/dto/create-purchase.dto.ts
+++ src/models/purchase/dto/create-purchase.dto.ts
@@ -14,7 +14,7 @@ export class CreatePurchaseDto extends OmitType(Purchase, [
   /** Product ID as UUID
    * @example "5c68ae94-bf3e-4fde-b01f-25d18b3976a0"
    */
-  @IsUUID()
+  @IsUUID(4)
   productId: string;
 
   /** Amount purchased of the product
