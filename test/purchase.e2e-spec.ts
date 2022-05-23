4ad94da4b0b91c14711bff30805ca8ee5defc457
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-05-23 13:06:05 -0300

    Test findOne, review, update and delete

--- test/purchase.e2e-spec.ts
+++ test/purchase.e2e-spec.ts
@@ -1,11 +1,19 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  INestApplication,
+  NotFoundException,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { isDate, isUUID } from 'class-validator';
 import { AppModule } from 'src/app.module';
+import { PurchaseNotFoundException } from 'src/common/exceptions/purchase/purchase-not-found.exception';
 import { ExceptionInterceptor } from 'src/common/interceptors/exception.interceptor';
 import { CreateProductDto } from 'src/models/product/dto/create-product.dto';
 import { CreatePurchaseDto } from 'src/models/purchase/dto/create-purchase.dto';
+import { ReviewPurchaseDto } from 'src/models/purchase/dto/review-purchase.dto';
+import { UpdatePurchaseDto } from 'src/models/purchase/dto/update-purchase.dto';
 import { Purchase } from 'src/models/purchase/entities/purchase.entity';
+import { NotPurchaseOwnerException } from 'src/models/purchase/exceptions/not-purchase-owner.exception';
 import { PrismaService } from 'src/prisma/prisma.service';
 import request from 'supertest';
 
@@ -296,6 +304,270 @@ describe('CategoryController (e2e)', () => {
 
     it('should not findAll user purchases if unauthenticated', async () => {
       await request(app.getHttpServer()).get('/purchase').send().expect(401);
+    });
+  });
+
+  describe('Get /purchase/:id', () => {
+    it('should get any purchase by id if admin', async () => {
+      const response = await request(app.getHttpServer())
+        .get(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${adminToken}` })
+        .send()
+        .expect(200);
+
+      const purchase: Purchase = response.body;
+
+      expect(purchase.id).toEqual(purchasesIds[2]);
+      expect(purchase.userId).toEqual(usersIds[0]);
+      expect(purchase.productId).toEqual(productsIds[1]);
+    });
+
+    it('should get user own purchases if not admin', async () => {
+      const response = await request(app.getHttpServer())
+        .get(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .send()
+        .expect(200);
+
+      const purchase: Purchase = response.body;
+
+      expect(purchase.id).toEqual(purchasesIds[2]);
+      expect(purchase.userId).toEqual(usersIds[0]);
+      expect(purchase.productId).toEqual(productsIds[1]);
+    });
+
+    it('should not get other users purchases if not admin', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .get(`/purchase/${purchasesIds[1]}`)
+          .set({ Authorization: `Bearer ${token}` })
+          .send()
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(
+          new NotFoundException(
+            new NotPurchaseOwnerException().message,
+          ).getResponse(),
+        ),
+      });
+    });
+
+    it('should not get purchase if id is invalid', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .get('/purchase/invalidId')
+          .set({ Authorization: `Bearer ${adminToken}` })
+          .send()
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(new PurchaseNotFoundException().getResponse()),
+      });
+    });
+
+    it('should not get purchase by id if unauthenticated', async () => {
+      await request(app.getHttpServer())
+        .get(`/purchase/${purchasesIds[2]}`)
+        .send()
+        .expect(401);
+    });
+  });
+
+  describe('Patch /purchase/review/:id', () => {
+    it('should review purchase', async () => {
+      const response = await request(app.getHttpServer())
+        .patch(`/purchase/review/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .send({
+          reviewNote: 5,
+          reviewComment: 'Amazing product!',
+        } as ReviewPurchaseDto)
+        .expect(200);
+
+      const purchase: Purchase = response.body;
+
+      expect(purchase.reviewComment).toEqual('Amazing product!');
+      expect(purchase.reviewNote).toEqual(5);
+
+      expect(purchase.id).toEqual(purchasesIds[2]);
+      expect(purchase.userId).toEqual(usersIds[0]);
+      expect(purchase.productId).toEqual(productsIds[1]);
+      expect(purchase.totalPrice).toEqual('250');
+      expect(purchase.amount).toEqual(3);
+    });
+
+    it('should not review other users purchases', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .patch(`/purchase/review/${purchasesIds[2]}`)
+          .set({ Authorization: `Bearer ${adminToken}` })
+          .send({
+            reviewNote: 1,
+            reviewComment: 'Not the purchase owner',
+          } as ReviewPurchaseDto)
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(
+          new NotFoundException(
+            new NotPurchaseOwnerException().message,
+          ).getResponse(),
+        ),
+      });
+    });
+
+    it('should not review purchase with invalid field', async () => {
+      await request(app.getHttpServer())
+        .patch(`/purchase/review/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .send({
+          userId: '2aff1e2a-faaf-4d58-9208-1db42e53cd57',
+          reviewNote: 5,
+          reviewComment: 'Amazing product!',
+        })
+        .expect(400);
+    });
+
+    it('should not review purchase if id is invalid', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .patch('/purchase/review/invalidId')
+          .set({ Authorization: `Bearer ${token}` })
+          .send({
+            reviewNote: 5,
+            reviewComment: 'Amazing product!',
+          } as ReviewPurchaseDto)
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(new PurchaseNotFoundException().getResponse()),
+      });
+    });
+
+    it('should not review purchase if unauthenticated', async () => {
+      await request(app.getHttpServer())
+        .patch(`/purchase/review/${purchasesIds[2]}`)
+        .send({
+          reviewNote: 5,
+          reviewComment: 'Amazing product!',
+        } as ReviewPurchaseDto)
+        .expect(401);
+    });
+  });
+
+  describe('Patch /purchase/:id', () => {
+    it('should update purchase', async () => {
+      const response = await request(app.getHttpServer())
+        .patch(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${adminToken}` })
+        .send({
+          productId: productsIds[0],
+          totalPrice: 300,
+          amount: 4,
+        } as UpdatePurchaseDto)
+        .expect(200);
+
+      const purchase: Purchase = response.body;
+
+      expect(purchase.id).toEqual(purchasesIds[2]);
+      expect(purchase.userId).toEqual(usersIds[0]);
+      expect(purchase.productId).toEqual(productsIds[0]);
+      expect(purchase.totalPrice).toEqual('300');
+      expect(purchase.amount).toEqual(4);
+      expect(purchase.reviewComment).toBeNull();
+      expect(purchase.reviewNote).toBeNull();
+    });
+
+    it('should not update invalid field', async () => {
+      await request(app.getHttpServer())
+        .patch(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${adminToken}` })
+        .send({
+          id: '4af7dbd4-bc93-4e37-b60f-67012b5d375b',
+          productId: productsIds[0],
+          totalPrice: 300,
+          amount: 4,
+        })
+        .expect(400);
+    });
+
+    it('should not update purchase if id is invalid', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .patch('/purchase/invalidId')
+          .set({ Authorization: `Bearer ${adminToken}` })
+          .send({
+            productId: productsIds[0],
+            totalPrice: 300,
+            amount: 4,
+          } as UpdatePurchaseDto)
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(new PurchaseNotFoundException().getResponse()),
+      });
+    });
+
+    it('should not update purchase id if user is not an admin', async () => {
+      await request(app.getHttpServer())
+        .patch(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .send({
+          productId: productsIds[0],
+          totalPrice: 300,
+          amount: 4,
+        } as UpdatePurchaseDto)
+        .expect(403);
+    });
+
+    it('should not update purchase id if unauthenticated', async () => {
+      await request(app.getHttpServer())
+        .patch(`/purchase/${purchasesIds[2]}`)
+        .send({
+          productId: productsIds[0],
+          totalPrice: 300,
+          amount: 4,
+        } as UpdatePurchaseDto)
+        .expect(401);
+    });
+  });
+
+  describe('Delete /purchase/:id', () => {
+    it('should delete purchase', async () => {
+      await request(app.getHttpServer())
+        .delete(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${adminToken}` })
+        .send()
+        .expect(204);
+
+      const databasePurchase = await prisma.purchase.findUnique({
+        where: { id: purchasesIds[2] },
+      });
+
+      expect(databasePurchase).toBeNull();
+    });
+
+    it('should not delete purchase if id is invalid', async () => {
+      await expect(
+        request(app.getHttpServer())
+          .delete('/purchase/invalidId')
+          .set({ Authorization: `Bearer ${adminToken}` })
+          .send()
+          .expect(404),
+      ).resolves.toMatchObject({
+        text: JSON.stringify(new PurchaseNotFoundException().getResponse()),
+      });
+    });
+
+    it('should not delete purchase if user is not an admin', async () => {
+      await request(app.getHttpServer())
+        .delete(`/purchase/${purchasesIds[2]}`)
+        .set({ Authorization: `Bearer ${token}` })
+        .send()
+        .expect(403);
+    });
+
+    it('should not delete purchase if unauthenticated', async () => {
+      await request(app.getHttpServer())
+        .delete(`/purchase/${purchasesIds[2]}`)
+        .send()
+        .expect(401);
     });
   });
 });
