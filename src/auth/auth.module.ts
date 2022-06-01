336957de772a5fdcf6af6ebe5f60ea5c556ccdd9
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-01 12:03:09 -0300

    Import PrismaModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from 'src/models/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { AccessJwtStrategy } from './access-jwt.strategy';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, PassportModule, JwtModule.register({})],
+  imports: [UserModule, PrismaModule, PassportModule, JwtModule.register({})],
   providers: [AuthService, AccessJwtStrategy],
   controllers: [AuthController],
 })
