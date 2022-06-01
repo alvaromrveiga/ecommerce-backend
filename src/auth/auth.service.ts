5b5d4886ee9e929c04a272abcfef19eb9af2fe74
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-01 12:05:16 -0300

    Add saveRefreshToken and logoutAll

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare } from 'bcrypt';
-import { accessJwtConfig } from 'src/config/jwt.config';
+import { compare, hash } from 'bcrypt';
+import { hashConfig } from 'src/config/hash.config';
+import { accessJwtConfig, refreshJwtConfig } from 'src/config/jwt.config';
 import { User } from 'src/models/user/entities/user.entity';
 import { UserService } from 'src/models/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { LoginResponse } from './dto/login.response';
 import { InvalidEmailOrPasswordError } from './errors/invalid-email-or-password.error.';
 
 /** Responsible for authenticating the user */
@@ -17,24 +20,40 @@ export class AuthService {
   constructor(
     private readonly userService: UserService,
     private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
   ) {}
 
   /** Validates if the inputted email exists and
    * compares if the hashed password matches the inputted one.
    *
-   * If so, returns the JWT accessToken
+   * If so, returns the access and refresh JWTs
    */
-  async login(
-    email: string,
-    password: string,
-  ): Promise<{ accessToken: string }> {
+  async login(email: string, password: string): Promise<LoginResponse> {
     const user = await this.validateUser(email, password);
 
     const payload = { sub: user.id, role: user.role };
 
+    const accessToken = await this.jwtService.signAsync(
+      payload,
+      accessJwtConfig,
+    );
+
+    const refreshToken = await this.jwtService.signAsync(
+      { sub: payload.sub },
+      refreshJwtConfig,
+    );
+
+    await this.saveRefreshToken(user.id, refreshToken);
+
     return {
-      accessToken: this.jwtService.sign(payload, accessJwtConfig),
+      accessToken,
+      refreshToken,
     };
+  }
+
+  /** Deletes all user's refresh tokens */
+  async logoutAll(userId: string): Promise<void> {
+    await this.prismaService.userTokens.deleteMany({ where: { userId } });
   }
 
   /** Validates if the inputted email exists and
@@ -54,5 +73,20 @@ export class AuthService {
     }
 
     throw new InvalidEmailOrPasswordError();
+  }
+
+  /** Saves the new refresh token hashed in the database */
+  private async saveRefreshToken(
+    userId: string,
+    newRefreshToken: string,
+  ): Promise<void> {
+    const hashedRefreshToken = await hash(
+      newRefreshToken,
+      hashConfig.saltRounds,
+    );
+
+    await this.prismaService.userTokens.create({
+      data: { userId, refreshToken: hashedRefreshToken },
+    });
   }
 }
