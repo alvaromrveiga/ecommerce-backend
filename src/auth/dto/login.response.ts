46e92909314ef1761c5e671ac949460e35597b90
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-01 12:03:52 -0300

    Add refresh token

--- src/auth/dto/login.response.ts
+++ src/auth/dto/login.response.ts
@@ -1,8 +1,14 @@
 /** Describes the response received when the Login route is successfully called */
 export class LoginResponse {
   /**
-   * JWT Authentication token
+   * Access JWT Authentication token
    * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI5NWFkMzNhNS0wYjk4LTQ2ODYtYjFmMS1hMTkwNzM0NWJmYzciLCJpYXQiOjE2NDg0NzU3MzEsImV4cCI6MTY0ODQ3NjYzMX0.h3z3JDvHOi6y5C_N0Kt6tdP2nWK_dHBZxioQn7VANNo"
    */
   accessToken: string;
+
+  /**
+   * Refresh JWT Authentication token
+   * @example "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+   */
+  refreshToken: string;
 }
--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -2,6 +2,12 @@ import { JwtSignOptions } from '@nestjs/jwt';
 
 /** Configurations for the access jsonwebtoken used for authentication */
 export const accessJwtConfig: JwtSignOptions = {
-  secret: process.env.JWT_SECRET,
+  secret: process.env.ACCESS_JWT_SECRET,
   expiresIn: '15m',
+};
+
+/** Configurations for the refresh jsonwebtoken used for authentication */
+export const refreshJwtConfig: JwtSignOptions = {
+  secret: process.env.REFRESH_JWT_SECRET,
+  expiresIn: '90d',
 };
