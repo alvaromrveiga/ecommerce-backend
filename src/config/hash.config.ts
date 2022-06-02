17aa0b4082af8b91716b2fc3097e618162d821ca
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-02 15:37:01 -0300

    Fix JSDoc comment

--- src/config/hash.config.ts
+++ src/config/hash.config.ts
@@ -1,4 +1,4 @@
-// Global configuration for amount of saltRounds on hashes of sensitive information
+/** Global configuration for amount of saltRounds on hashes of sensitive information */
 export const hashConfig = {
   saltRounds: 10,
 };
