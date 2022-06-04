563f3cd3975a2274a8169fb9ab046c1245a95ea4
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-04 12:36:41 -0300

    Handle auth service expcetions

--- src/common/interceptors/handlers/user-input-exception.handler.ts
+++ src/common/interceptors/handlers/user-input-exception.handler.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { AuthServiceInputException } from 'src/auth/exceptions/auth-service-input.exception';
 import { ProductServiceInputException } from 'src/models/product/exceptions/product-service-input.exception';
 import { PurchaseServiceInputException } from 'src/models/purchase/exceptions/purchase-service-input.exception';
 import { UserServiceInputException } from 'src/models/user/exceptions/user-service-input.exception';
@@ -12,6 +17,10 @@ export class UserInputExceptionHandler implements ExceptionHandler {
    * respective HTTP error
    */
   handle(error: Error): void {
+    if (error instanceof AuthServiceInputException) {
+      throw new UnauthorizedException(error.message);
+    }
+
     if (error instanceof UserServiceInputException) {
       throw new BadRequestException(error.message);
     }
