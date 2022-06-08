9a36261678223eb820634e72a9b63bbf667d9e59
Álvaro Veiga <alvaromrveiga@gmail.com>
2022-06-08 11:40:12 -0300

    Remove total price

--- src/models/purchase/dto/create-purchase.dto.ts
+++ src/models/purchase/dto/create-purchase.dto.ts
@@ -1,15 +1,11 @@
-import { OmitType } from '@nestjs/swagger';
-import { Decimal } from '@prisma/client/runtime';
+import { PickType } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsPositive, IsUUID } from 'class-validator';
 import { Purchase } from '../entities/purchase.entity';
 
 /** Describes the fields needed to create a Purchase */
-export class CreatePurchaseDto extends OmitType(Purchase, [
-  'id',
-  'userId',
-  'createdAt',
-  'reviewNote',
-  'reviewComment',
+export class CreatePurchaseDto extends PickType(Purchase, [
+  'productId',
+  'amount',
 ]) {
   /** Product ID as UUID
    * @example "5c68ae94-bf3e-4fde-b01f-25d18b3976a0"
@@ -24,14 +20,5 @@ export class CreatePurchaseDto extends OmitType(Purchase, [
   @IsNumber()
   @IsPositive()
   @IsOptional()
-  amount?: number;
-
-  /** Price payed per product multiplied by the amount
-   * Saved as decimal, calculations should be handled
-   * with currency.js
-   * @example 138.75
-   */
-  @IsNumber()
-  @IsPositive()
-  totalPrice: string | number | Decimal;
+  amount: number;
 }
